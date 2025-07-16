7ab218820d24a87624bfe5427f0e5e6e8c5537b1
Parthsaarthie Sharma <parthsaarthiesharma1909@yahoo.com>
2025-07-16 12:51:04 +0530

    COMMIT

--- lib/screens/filters.dart
+++ lib/screens/filters.dart
@@ -26,7 +26,7 @@ class FiltersScreen extends ConsumerWidget {
             title: Text(
               'Gluten-free',
               style: Theme.of(context).textTheme.titleLarge!.copyWith(
-                color: Theme.of(context).colorScheme.onBackground,
+                color: Theme.of(context).colorScheme.onSurface,
               ),
             ),
             subtitle: Text(
