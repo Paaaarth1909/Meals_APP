a58b469502fd664530b617e57eb9cd102ce87850
Parthsaarthie Sharma <parthsaarthiesharma1909@yahoo.com>
2025-07-16 12:27:40 +0530

    COMMIT

--- lib/widgets/meal_item.dart
+++ lib/widgets/meal_item.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:transparent_image/transparent_image.dart';
 
 import 'package:meals/widgets/meal_item_trait.dart';
-import 'package:meals/models/meal.dart';
+import 'package:meals_app/models/meal.dart';
 
 class MealItem extends StatelessWidget {
   const MealItem({super.key, required this.meal, required this.onSelectMeal});
