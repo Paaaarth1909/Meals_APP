8e7230c38b76bd1787cdf037f16c84556cda320a
Parthsaarthie Sharma <parthsaarthiesharma1909@yahoo.com>
2025-07-16 12:46:26 +0530

    COMMIT

--- lib/screens/meals.dart
+++ lib/screens/meals.dart
@@ -32,7 +32,7 @@ class MealsScreen extends StatelessWidget {
           Text(
             'Try selecting a different category!',
             style: Theme.of(context).textTheme.bodyLarge!.copyWith(
-              color: Theme.of(context).colorScheme.onBackground,
+              color: Theme.of(context).colorScheme.onSurface,
             ),
           ),
         ],
