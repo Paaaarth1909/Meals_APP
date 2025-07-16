c0e99eded0c919ef201ca1001caa4d32df0a4001
Parthsaarthie Sharma <parthsaarthiesharma1909@yahoo.com>
2025-07-16 12:21:11 +0530

    COMMIT

--- lib/screens/categories.dart
+++ lib/screens/categories.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 import 'package:meals/data/dummy_data.dart';
-import 'package:meals/models/meal.dart';
+import 'package:meals_app/models/meal.dart';
 import 'package:meals_app/widgets/category_grid_item.dart';
 import 'package:meals_app/screens/meals.dart';
 import 'package:meals_app/models/category.dart';
