61cc91e4badfebb0efad066454b56338c8b23750
Parthsaarthie Sharma <parthsaarthiesharma1909@yahoo.com>
2025-07-16 12:17:39 +0530

    COMMIT

--- lib/data/dummy_data.dart
+++ lib/data/dummy_data.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-import 'package:meals/models/category.dart';
+import 'package:meals_app/models/category.dart';
 import 'package:meals_app/models/meal.dart';
 
 // Constants in Dart should be written in lowerCamelcase.
