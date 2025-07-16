1cc1147031b7303691427eae621549c1dd63246f
Parthsaarthie Sharma <parthsaarthiesharma1909@yahoo.com>
2025-07-16 12:26:39 +0530

    COMMIT

--- lib/screens/tabs.dart
+++ lib/screens/tabs.dart
@@ -6,7 +6,7 @@ import 'package:meals_app/screens/filters.dart';
 import 'package:meals_app/screens/meals.dart';
 import 'package:meals_app/widgets/main_drawer.dart';
 import 'package:meals_app/providers/favorites_provider.dart';
-import 'package:meals/providers/filters_provider.dart';
+import 'package:meals_app/providers/filters_provider.dart';
 
 const kInitialFilters = {
   Filter.glutenFree: false,
