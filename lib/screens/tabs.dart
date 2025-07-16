257f97d07638da7c4d034e79f6c1788d89f9fc04
Parthsaarthie Sharma <parthsaarthiesharma1909@yahoo.com>
2025-07-16 12:25:27 +0530

    COMMIT

--- lib/screens/tabs.dart
+++ lib/screens/tabs.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 import 'package:meals_app/screens/categories.dart';
-import 'package:meals/screens/filters.dart';
+import 'package:meals_app/screens/filters.dart';
 import 'package:meals/screens/meals.dart';
 import 'package:meals/widgets/main_drawer.dart';
 import 'package:meals/providers/favorites_provider.dart';
